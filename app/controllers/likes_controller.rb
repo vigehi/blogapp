fe3efa8a7b95d771dfd84b09046fc42a7e59cb71
Dennis <vigehi2017@gmail.com>
2022-12-07 17:49:52 +0300

    Updates on controllers

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,13 @@
+class LikesController < ApplicationController
+ 
+  def create
+    @post = Post.find(params[:id])
+
+    return if @post.liked?(current_user)
+
+    @like = Like.create(author_id: current_user.id, post_id: @post.id)
+
+    redirect_to user_post_path(current_user, @post)
+  end
+
+end
