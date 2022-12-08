d2be8c11dbc6d105c724a021def6cf4436272f6e
Dennis <vigehi2017@gmail.com>
2022-12-08 10:09:40 +0300

    Updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 end
