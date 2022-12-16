5723956367288ace3a14f6c33c9c3eeea6535b5e
Dennis <vigehi2017@gmail.com>
2022-12-16 16:30:33 +0300

    updates

--- db/migrate/20221213125259_devise_create_models.rb
+++ db/migrate/20221213125259_devise_create_models.rb
@@ -2,9 +2,9 @@
 
 class DeviseCreateModels < ActiveRecord::Migration[7.0]
   def change
-    create_table :models do |t|
+    create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email, null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -36,8 +36,8 @@ class DeviseCreateModels < ActiveRecord::Migration[7.0]
       t.timestamps null: false
     end
 
-    add_index :models, :email,                unique: true
-    add_index :models, :reset_password_token, unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
     # add_index :models, :confirmation_token,   unique: true
     # add_index :models, :unlock_token,         unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_13_125259) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_16_222607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,7 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_125259) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
