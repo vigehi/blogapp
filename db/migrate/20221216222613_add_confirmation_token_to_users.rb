4235a7217ddfa81cfc5c86ca889ae7474f3d38ac
andym80 <andymenutti@gmail.com>
2022-12-16 16:48:58 -0300

    fix postgres superuser

--- db/migrate/20221216222613_add_confirmation_token_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddConfirmationTokenToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :confirmation_token, :string
-  end
-  add_index :users, :confirmation_token, unique: true
-
-end
